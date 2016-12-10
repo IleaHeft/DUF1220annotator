8c60e8556bc5119efcf141ac813b085be0776894
Ilea Heft <ilea.eskildsen@ucdenver.edu>
2016-12-09 17:55:13 -0700

    add config.sh

---
+++ scripts/config.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Set variables for HMMER annotation workflow
+
+# RefSeq files
+
+refseq_gff3=data/reference-annotations/refseq/GCF_000001405.35_GRCh38.p9_genomic.gff
+refseq_assembly_report=data/reference-annotations/refseq/GCF_000001405.35_GRCh38.p9_assembly_report.txt
+
+
+# Ensembl GTF file
+ensembl_gtf=data/reference-annotations/ensembl/Homo_sapiens.GRCh38.86.gtf
+
